04d2cfb41b1b74d0c70c6fd10985ed74f48f34a9
Marcelo Pinto <pintomarc@gmail.com>
2023-07-13 01:52:15 -0300

    ajustes pré homologacao

--- publisher/publicadorRBMQ.py
+++ publisher/publicadorRBMQ.py
@@ -1,70 +1,27 @@
-#import classeRabbit as rbmq
-import json 
-
-# replica o caracter a quantdade de vezes desejada
-def repl(c, n):
-    ret = ""
-    for x in range(n):
-        ret += c
-    return (ret)
-
-# monta o menu de e retorna a opção escolhida para envio da mensagem
-def sendTo():
-    #clear
-    vMenu = [-1,1,2,3]
-    print (repl("=",40))
-    print ("| 1 - Enviar mensagem para RabbitMq    |")
-    print ("| 2 - Enviar mensagem para Kafka       |")
-    print ("| 3 - Enviar mensagem para MQTT        |")
-    print (repl("-",40))
-    print ("Escolha o número dentre as opções acima ou -1 para sair")
-    try:
-        vOpt = int(input(""))
-        while vOpt not in vMenu:
-            print ("Escolha o número dentre as opções acima ou -1 para sair.")
-            vOpt = int(input(""))
-        return (vOpt)
-    except:
-        return(-1)
-    
-def montaPayload (pContext, pMsg):
-    return ({
-        "contexto": pContext, 
-        "mensagem": pMsg
-            })    
-
-# Obtem o contexto e mensagem para montagem do payload
-vContext = input("Informe o contexto da mensagem: ")
-vMsg = input("Informe a mensagem a ser enviada: ")
-
-payload = montaPayload(vContext,vMsg)
-print()
-print (payload)
-print()
-
-#!/usr/bin/env python
+import json
 import pika
 
-host='192.168.0.25'
-port='15672'
-user='guest'
-passwd='guest'
+host="localhost"
+port=15672
+user="guest"
+passwd="guest"
+vhost="middleware" 
 
 credenciais = pika.PlainCredentials(user,passwd)
 
-conexao = pika.BlockingConnection(pika.ConnectionParameters(host=host, credentials=credenciais))
+conexao = pika.BlockingConnection(pika.ConnectionParameters(host=host, virtual_host="middleware", credentials=credenciais))
 
+#print("conectado...")
+#print()
 channel = conexao.channel()
+#channel.queue_declare(queue='geral')
+def pubMensRBMQ(mensagem)
+    exchange=mensagem["contexto"]
+    routk=mensagem["contexto"]
+    channel.basic_publish(exchange=exchange,
+                    routing_key=routk,
+                    body=mensagem)
 
-channel.queue_declare(queue='geral')
-
-channel.basic_publish(exchange='distrib',
-                      routing_key='geral',
-                      body='Hello World!')
-
-print(" [x] enviei 'Hello World!'")
-conexao.close()
-
-
-
+def desconectarRBMQ()    
+    conexao.close()
 
