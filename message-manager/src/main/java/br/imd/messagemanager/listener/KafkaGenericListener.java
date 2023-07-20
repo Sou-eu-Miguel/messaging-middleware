a9f8f881c455a256bb84f4937bfac22e43ac4510
Miguel Brilhante <miguel.brilhantesilva-ext@ab-inbev.com>
2023-07-20 20:25:45 -0300

    Added log in rabbitConsumer

--- message-manager/src/main/java/br/imd/messagemanager/listener/KafkaGenericListener.java
+++ message-manager/src/main/java/br/imd/messagemanager/listener/KafkaGenericListener.java
@@ -4,12 +4,14 @@ import br.imd.messagemanager.model.MiddlewareMessage;
 import br.imd.messagemanager.publisher.PublisherBus;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaGenericListener {
-
+  private static final Logger LOGGER = LogManager.getLogger(MosquitoListener.class);
   private final ObjectMapper objectMapper;
   private final PublisherBus publisherBus;
 
@@ -21,6 +23,7 @@ public class KafkaGenericListener {
   @KafkaListener(topics = "#{channelProperty.generic.name()}", groupId = "#{channelProperty.generic.name()}")
   public void receiveMessage(String message) throws JsonProcessingException {
     final var middlewareMessage = objectMapper.readValue(message, MiddlewareMessage.class);
+    LOGGER.info("Received message: {}", message);
     publisherBus.send(middlewareMessage);
   }
 }
--- message-manager/src/main/java/br/imd/messagemanager/listener/RabbitMQListener.java
+++ message-manager/src/main/java/br/imd/messagemanager/listener/RabbitMQListener.java
@@ -2,12 +2,15 @@ package br.imd.messagemanager.listener;
 
 import br.imd.messagemanager.model.MiddlewareMessage;
 import br.imd.messagemanager.publisher.PublisherBus;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RabbitMQListener {
+  private static final Logger LOGGER = LogManager.getLogger(RabbitMQListener.class);
 
   private final PublisherBus publisherBus;
 
@@ -17,6 +20,7 @@ public class RabbitMQListener {
 
   @RabbitListener(queues = "#{channelProperty.generic.name()}")
   public void handler(@Payload MiddlewareMessage message) {
+    LOGGER.info("Received message: {}", message);
     publisherBus.send(message);
   }
 }
