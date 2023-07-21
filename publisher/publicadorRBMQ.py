aa413da573d3c84016f766635a6128c9f047bdee
Marcelo Pinto <pintomarc@gmail.com>
2023-07-20 22:18:05 -0300

    Atualização dos arquivos pós validação das rotinas principais

--- publisher/publicadorRBMQ.py
+++ publisher/publicadorRBMQ.py
@@ -6,22 +6,32 @@ port=15672
 user="guest"
 passwd="guest"
 vhost="middleware" 
+vexchange="generic"
 
 credenciais = pika.PlainCredentials(user,passwd)
 
-conexao = pika.BlockingConnection(pika.ConnectionParameters(host=host, virtual_host="middleware", credentials=credenciais))
+conexao = pika.BlockingConnection(pika.ConnectionParameters(host=host, virtual_host=vhost, credentials=credenciais))
 
 #print("conectado...")
 #print()
 channel = conexao.channel()
-#channel.queue_declare(queue='geral')
+#channel.queue_declare(queue='generic',durable=False)
 def pubMensRBMQ(mensagem):
-    exchange=mensagem["contexto"]
-    routk=mensagem["contexto"]
-    channel.basic_publish(exchange=exchange,
-                    routing_key=routk,
-                    body=mensagem)
+    exchange=vexchange
+    routk="*"
+    channel.basic_publish(exchange=exchange,routing_key=routk,body=mensagem)
+    #channel.basic_publish(exchange=exchange,body=mensagem)
+
 
 def desconectarRBMQ():    
     conexao.close()
+
+vmensagem = json.dumps({
+        "context": "vasco", 
+        "body": "Ta indo para segunda..."
+            })
+
+pubMensRBMQ(mensagem=vmensagem)
+
+
 
